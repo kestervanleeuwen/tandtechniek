18b2c658f519eafae8923fbe2cf7ae2807e2a0b6
Kester van Leeuwen <kester@posonas.com>
2022-07-01 12:39:26 +0200

    quickfix

--- src/main/java/hu/tandtechniek/application/data/VoorraadRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.tandtechniek.application.data;
-
-import hu.tandtechniek.application.domain.Tandtechnicus;
-import hu.tandtechniek.application.domain.Voorraad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoorraadRepository extends JpaRepository<VoorraadRepository, String> {
-    Voorraad findByVoorraadId(int id);
-}
